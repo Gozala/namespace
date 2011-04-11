b6a927c989ae812609849de495b5a2c1a59832c5
Irakli Gozalishvili <rfobic@gmail.com>
2011-04-11 08:38:14 +0200

    Init.

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# namespace #
+
+Library for defining namespaced properties.
+
+## Install ##
+
+    npm install namespace
+
---
+++ test/test-namespace.js
@@ -0,0 +1,7 @@
+"use strict";
+
+exports['test namsepace'] = function() {
+};
+
+if (module == require.main)
+  require('test').run(exports);
